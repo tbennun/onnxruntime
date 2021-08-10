645ec541e3e252005105557ee0733cac2762dc95
aishwarya bhandare <aibhanda@microsoft.com>
2021-08-10 21:38:16 +0000

    Review comments

--- orttraining/orttraining/python/training/ortmodule/_training_manager.py
+++ orttraining/orttraining/python/training/ortmodule/_training_manager.py
@@ -42,15 +42,10 @@ class TrainingManager(GraphExecutionManager):
         forward_outputs = C.OrtValueVector()
         # Run and return module outputs.
         execution_session.run_forward(forward_inputs, forward_outputs, state, cache)
-
-        user_outputs = []
-        for i in range(len(forward_outputs)):
-            if i < cache_start:
-                user_outputs.append(_utils._ortvalue_to_torch_tensor(forward_outputs[i]))
-            elif cache !=None:
+        user_outputs = tuple(_utils._ortvalue_to_torch_tensor(forward_outputs[i]) for i in range(cache_start))
+        if cache != None:
+            for i in range(cache_start, len(forward_outputs)):
                 cache.insert(cache_names[i-cache_start], forward_outputs[i])
-
-        user_outputs = tuple(user_outputs)
 
         output_info = [(output.shape, output.device, output.dtype) for output in user_outputs]
         run_info = RunStateInfo(state, output_info)
@@ -149,6 +144,7 @@ class TrainingManager(GraphExecutionManager):
                     _utils._check_same_device(self._device, "Input argument to forward", *inputs)
 
                 user_outputs, ctx.run_info = TrainingManager.execution_session_run_forward(self._execution_agent,
+                                                                                           self._onnx_models.optimized_model,
                                                                                            self._cache,
                                                                                            self._graph_info.cached_node_arg_name,
                                                                                            len(self._graph_info.user_output_names),
